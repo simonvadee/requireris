ca360642fbf97b5726e6e6846b6db2fd431c0e91
verove-j <jordan.verove@epitech.eu>
2015-12-07 21:01:28 +0100

    ajout boutton submit/back

--- sources/reqGUI.py
+++ sources/reqGUI.py
@@ -19,8 +19,14 @@ class reqGUI(object):
         for child in self.window.winfo_children():
             child.destroy()
 
-        frameInfo = Frame(self.window,borderwidth = 0, relief = GROOVE)
-        frameInfo.pack(side = LEFT, padx = 10, pady = 30)
+        frameBack = Frame(self.window, borderwidth = 1, relief = GROOVE)
+        frameBack.pack(side = TOP, padx = 10)
+            
+        previousButton = Button(frameBack, text = "back")
+        previousButton.pack(fill = X)
+
+        frameInfo = Frame(self.window,borderwidth = 1, relief = GROOVE)
+        frameInfo.pack(side = LEFT, padx = 0, expand = True)
 
         accNameLabel = Label(frameInfo, text = "Name Account")
         accNameLabel.pack()
@@ -29,17 +35,28 @@ class reqGUI(object):
         accConfirmPassLabel = Label(frameInfo, text = "Confirm Password")
         accConfirmPassLabel.pack()
 
-        frameFields = Frame(self.window, borderwidth = 0, relief = GROOVE)
+        frameFields = Frame(self.window, borderwidth = 1, relief = GROOVE)
         frameFields.pack(side = RIGHT, padx = 10, pady = 30)
 
-        nameAccEntry = Entry(frameFields, relief = GROOVE)
-        nameAccEntry.pack()
-        passEntry = Entry(frameFields, relief = GROOVE, show = "*")
-        passEntry.pack()
-        confirmPassEntry = Entry(frameFields, relief = GROOVE, show = "*")
-        confirmPassEntry.pack()
+        self.nameAccEntry = Entry(frameFields, relief = GROOVE)
+        self.nameAccEntry.pack()
+        self.passEntry = Entry(frameFields, relief = GROOVE, show = "*")
+        self.passEntry.pack()
+        self.confirmPassEntry = Entry(frameFields, relief = GROOVE, show = "*")
+        self.confirmPassEntry.pack()
+
+        frameSubmit = Frame(self.window, borderwidth = 0, relief = GROOVE)
+        frameSubmit.pack(side = BOTTOM)
+        
+        submitButton = Button(frameSubmit, text = "Submit", command = self.createNewAcc)
+        submitButton.pack()
+
+    def initChooseAccView(self):
+        print("Choose Account View")
         
     def initLogView(self):
+        for child in self.window.winfo_children():
+            child.destroy()
         frameGenOTP = Frame(self.window, borderwidth = 0, relief = GROOVE)
         frameGenOTP.pack(side = LEFT, padx = 20, pady = 50)
         genOTPButton = Button(frameGenOTP, text = "Create new account", command = self.initCreateAccView)
@@ -47,5 +64,9 @@ class reqGUI(object):
 
         frameUseExistingAcc = Frame(self.window, borderwidth = 0, relief = GROOVE)
         frameUseExistingAcc.pack(side = RIGHT, padx = 10, pady = 50)
-        useExistingAccButton = Button(frameUseExistingAcc, text = "Use existing account", fg = "black")
+        useExistingAccButton = Button(frameUseExistingAcc, text = "Use existing account", command = self.initChooseAccView)
         useExistingAccButton.pack()
+
+    def createNewAcc(self):
+        if self.nameAccEntry.get() != "" and self.passEntry.get() != "" and self.confirmPassEntry.get() != "":
+            print("nameAcc = " + self.nameAccEntry.get() + " password : " + self.passEntry.get())
