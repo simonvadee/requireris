0ca12321f4e0eb1c1552b7c6bbfe3be4ca385e47
verove-j <jordan.verove@epitech.eu>
2015-12-12 21:14:43 +0100

    gui terminée

--- sources/reqGUI.py
+++ sources/reqGUI.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
  
-from tkinter import * 
+from tkinter import *
+from tkinter import messagebox
 
 class reqGUI(object):
     """
@@ -9,6 +10,7 @@ class reqGUI(object):
 
     def __init__(self, manager):
         self.manager = manager
+        self.wrongpassword = 0
         self.window = Tk()
         self.window.title("Requireris")
         self.window.geometry("590x420")
@@ -66,7 +68,7 @@ class reqGUI(object):
         previousButton = Button(frameBack, text = "back", command = self.initLogView)
         previousButton.pack(fill = X)
 
-        frameInfo = Frame(self.window,borderwidth = 1, relief = GROOVE)
+        frameInfo = Frame(self.window,borderwidth = 0, relief = GROOVE)
         frameInfo.pack(side = LEFT, padx = 0, expand = True)
 
         accNameLabel = Label(frameInfo, text = "Name Account")
@@ -90,31 +92,16 @@ class reqGUI(object):
         
     def closePopup(self):
         self.popup.destroy()
-            
-    def genNewSeedView(self):
-        for child in self.popup.winfo_children():
-            child.destroy()
-        frameNewSeed = Frame(self.popup, borderwidth = 0, relief = GROOVE)
-        frameNewSeed.pack(padx = 40, pady = 40)
-        newSeedEntry = Entry(frameNewSeed, text = "gen new seed here", state = 'normal')
-        newSeedEntry.insert(0, "gen new seed here")
-        newSeedEntry.configure(state = 'disabled')
-        newSeedEntry.pack()
-#ICI
-        doneButton = Button(self.popup, text = "done", borderwidth = 1, relief = GROOVE, command = self.closePopup)
-        doneButton.pack()
 
     def initLogView(self):
+        self.wrongpassword = 0
         for child in self.window.winfo_children():
             child.destroy()
         frameGenOTP = Frame(self.window, borderwidth = 0, relief = GROOVE)
         frameGenOTP.pack(side = RIGHT, padx = 20, pady = 50)
-#        frameGenOTP.pack(side = LEFT, padx = 20, pady = 50)
-        genOTPButton = Button(frameGenOTP, text = "Create new account", command = self.initCreateAccView)
-        genOTPButton.pack()
+        createNewAccButton = Button(frameGenOTP, text = "Create new account", command = self.initCreateAccView)
+        createNewAccButton.pack()
 
-#        frameUseExistingAcc = Frame(self.window, borderwidth = 0, relief = GROOVE)
- #       frameUseExistingAcc.pack(side = RIGHT, padx = 10, pady = 50)
         useExistingAccButton = Button(frameGenOTP, text = "Use existing account", command = self.initChooseAccView)
 
         frameAccounts = Frame(self.window, borderwidth = 0, relief = GROOVE)
@@ -129,31 +116,36 @@ class reqGUI(object):
         
         def modifyDelete(accountName):
             def execModifyButton(account):
-                #self.manager.updateKey(account)
                 for child in self.popup.winfo_children():
                     child.destroy()
+                self.currAccount = accountName
                 frameField = Frame(self.popup, borderwidth = 0, relief = GROOVE)
-                frameField.pack(side = LEFT, padx = 20, pady = 20)
+                frameField.pack(side = LEFT, padx = 0, pady = 20)
                 passLabel = Label(frameField, text = "Password")
                 passLabel.pack()
-
+                seedLabel = Label(frameField, text = "Seed")
+                seedLabel.pack()
                 framePassword = Frame(self.popup, borderwidth = 0, relief = GROOVE)
-                framePassword.pack(side = RIGHT, padx = 20, pady = 20)
-                passEntry = Entry(framePassword, relief = GROOVE)
-                passEntry.pack()
-                submitButton = Button(framePassword, text = "submit",  borderwidth = 1, relief = GROOVE, command = self.genNewSeedView)
+                framePassword.pack(side = RIGHT, padx = 5, pady = 20)
+                self.passEntry = Entry(framePassword, relief = GROOVE, show = "*")
+                self.passEntry.pack()
+                self.seedEntry = Entry(framePassword, relief = GROOVE)
+                self.seedEntry.pack()
+                frameSubmit = Frame(self.popup, borderwidth = 0, relief = GROOVE)
+                frameSubmit.pack(side = BOTTOM)
+                submitButton = Button(frameSubmit, text = "submit",  borderwidth = 1, relief = GROOVE, command = self.genOTPView)
                 submitButton.pack()
-                #verifier que le mot de passe est bon
                 
                 
-            def excDeleteButton(account):
-                self.manager.updateKey(account)
-                self.initLogView()
-
+            def execDeleteButton(account):
+                if messagebox.askyesno('Delete', 'Are you sure you want to delete this account ?'):
+                    self.manager.deleteAccount(account)
+                    self.popup.destroy()
+                
             print("account name = ")
             print(accountName)
             self.popup = Tk()
-            self.popup.geometry("200x200")
+            self.popup.geometry("400x300")
             self.popup.wm_title("Requireris")
             B1 = Button(self.popup, text = "Modify", command = lambda account=accountName: execModifyButton(account))
             B1.pack()
@@ -165,9 +157,35 @@ class reqGUI(object):
             print(account)
             b.pack()
 
-    def modify(self):
-      #  self.manager.updateKey()
-        print("okok")
+    def genOTPView(self):
+        if not self.manager.openExistingSession(self.currAccount,
+                                                   self.passEntry.get()):
+            if self.wrongpassword == 0:
+                errorLabel = Label(self.popup, text = "WRONG PASSWORD", fg = "red")
+                errorLabel.pack();
+                self.wrongpassword = 1
+            pass
+        else:
+            self.wrongpassword = 0
+            self.currPassword = self.passEntry.get()
+            self.currSeed = self.seedEntry.get()
+            for child in self.popup.winfo_children():
+                child.destroy()
+            self.otp = self.manager.updateKey(self.currAccount, self.currPassword, self.currSeed)
+            frameNewSeed = Frame(self.popup, borderwidth = 0, relief = GROOVE)
+            frameNewSeed.pack(padx = 40, pady = 40)
+            self.newOTP = Entry(frameNewSeed, state = 'normal')
+            self.newOTP.insert(0, self.otp)
+            self.newOTP.pack()
+            updateButton = Button(self.popup, text = "Refresh", borderwidth = 1, relief = GROOVE, command = self.updateOTP)
+            updateButton.pack()
+            doneButton = Button(self.popup, text = "done", borderwidth = 1, relief = GROOVE, command = self.closePopup)
+            doneButton.pack()
+
+    def updateOTP(self):
+        self.otp = self.manager.updateKey(self.currAccount, self.currPassword, self.currSeed)
+        self.newOTP.delete(0, END)
+        self.newOTP.insert(0, self.otp)
         
     def createNewAcc(self):
         if self.nameAccEntry.get() != "" and self.passEntry.get() != "" and self.confirmPassEntry.get() != "":
@@ -176,6 +194,7 @@ class reqGUI(object):
                                                 self.confirmPassEntry.get(),
                                                 self.seedEntry.get()):
                 # erreur : mettre le emssage d'erreur approprié
+                # je sais pas quel message d'erreur mettre ?
                 pass
             else:
                 # on est connecté, revenir au menu et afficher le code avec un petit compte à rebourd et réactualiser ?
@@ -187,8 +206,13 @@ class reqGUI(object):
             if not self.manager.openExistingSession(self.nameAccEntry.get(),
                                                    self.passEntry.get()):
                 # wrong password, afficher erreur !!
+                if self.wrongpassword == 0:
+                    errorLabel = Label(self.window, text = "WRONG PASSWORD", fg = "red")
+                    errorLabel.pack();
+                    self.wrongpassword = 1
                 pass
             else:
                 # on est connecté, revenir au menu et afficher le code avec un petit compte à rebourd et réactualiser ?
                 print("success !")
+                self.wrongpassword = 0
                 self.initLogView()
